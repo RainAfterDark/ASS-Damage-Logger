fbfee6f4146ad9a83444a8d5661a841eb31b1a52
雨 <42061128+RainAfterDark@users.noreply.github.com>
2022-11-08 16:29:30 +0800

    Update damage_parser.py

--- damage_parser.py
+++ damage_parser.py
@@ -1,11 +1,9 @@
+#!/usr/bin/env python3
 # -------------------------------------------------------------------------------------- #
 # Simple damage parser py script by Ame                                                  #
-#                                                                                        #
-# Usage: Provide a log file path either in the script args or in the console input.      #
-#        Alternatively, you can just directly paste logs into the console.               #
-#                                                                                        #
-# Script args: <log file path> <use reaction correction>                                 #
-#               str, required   Y/N, default N                                           #
+# Usage: Provide a log file path or directly paste logs into the console input.          #
+# Optional flags: -rc Y/N                                                                #
+#                 sets global setting for using reaction correction (stops prompts)      #
 # -------------------------------------------------------------------------------------- #
 #   Basically just outputs a damage breakdown and reaction counts for a specified log.   #
 # Reaction correction has varying levels of accuracy, as it really only does frequency   #
@@ -19,34 +17,32 @@
 # character amplified, and thus doesn't count towards their total damage number.         #
 # -------------------------------------------------------------------------------------- #
 
-import sys, re, traceback, logging
+import argparse, re, traceback, logging
 from collections import defaultdict
+from enum import IntEnum
 
-class col:
+class col(IntEnum):
     type, uid, time, delta, source, attacker, damage, crit, eda, \
-    element, reaction, amp_type, amp_rate, count, aid, mid, defender = range(17)
+    element, reaction, amp_type, amp_rate, count, aid, mid, defender \
+    = range(17)
 
 # we ignore blooms as they're already directly associated to the source
 reaction_blacklist = ("None", "Bloom", "BountifulBloom", "Hyperbloom")
 
-args = sys.argv[1:]
-
-def arg(i, default = None):
-    return args[i] if len(args) > i else default
-
 def str2bool(str):
     return str.lower() in ("true", "t", "yes", "y", "1")
+
+def dict2str(d):
+    return ", ".join((f"{k}: {d[k]}" for k in sorted(d, key=d.get, reverse=True)))
 
 def try_open(path):
     lines, err = [], None
     try:
         with open(path, "r") as file:
             lines = file.read().splitlines()
-    except Exception as e:
-        err = e
-    finally:
-        fname = re.split(r"\/|\\", path)[-1] if path else None
-        return fname, lines, err
+    except Exception as e: err = e
+    fname = re.split(r"\/|\\", path)[-1] if path else None
+    return fname, lines, err
 
 def nested_dd(depth, default_factory):
     return defaultdict(default_factory if depth < 1
@@ -58,28 +54,28 @@ def damage_check(row):
     and re.search(r"[A-Z]+\d+", row[col.defender].split(" ")[0]))
 
 def parse_log(lines, use_correction):
-    occurence_table = nested_dd(2, int) # reaction (str): damage (float): attacker (str): frequency (int)
+    frequency_table = nested_dd(2, int) # reaction → damage → attacker → frequency
+    for line in lines:
+        row = line.split(",")
+        if not damage_check(row): continue
 
-    if use_correction:
-        for line in lines:
-            row = line.split(",")
-            if not damage_check(row): continue
+        reaction = row[col.reaction]
+        if reaction in reaction_blacklist: continue
 
-            reaction = row[col.reaction]
-            if reaction in reaction_blacklist: continue
+        damage = float(row[col.damage])
+        attacker = row[col.attacker]
+        frequency_table[reaction][damage][attacker] += 1
 
-            damage = float(row[col.damage])
-            attacker = row[col.attacker]
-            occurence_table[reaction][damage][attacker] += 1
-
-        print("\n\tReaction Occurences:")
-        for reaction, damage_table in occurence_table.items():
-            for damage, occurences in damage_table.items():
-                print(f"\t{reaction}: {damage} -> {dict(occurences)}")
+    if frequency_table:
+        print("\nReaction Frequency:")
+        print("\n".join(f"{reaction} {damage} = {dict2str(frequency)}"
+        for reaction, damage_table in frequency_table.items()
+        for damage, frequency in damage_table.items()))
 
     total_damage, total_time = 0, -1
-    damage_table = nested_dd(2, int) # attacker (str): stat (str): defaultdict(int)
-    ownership_table = nested_dd(1, int) # reaction (str): avatar (str): count (int)
+    damage_table = nested_dd(2, int) # attacker → stat → defaultdict(int)
+    ownership_table = nested_dd(1, int) # reaction → avatar → owned_count (int)
+    corrected = False
 
     for line in lines:
         row = line.split(",")
@@ -93,36 +89,41 @@ def parse_log(lines, use_correction):
         damage_table[attacker]["Total"]["dmg"] += damage
         total_damage += damage
 
+        def add2dt(stat):
+            damage_table[attacker][stat]["dmg"] += damage
+            damage_table[attacker][stat]["count"] += 1
+
         reaction = row[col.reaction]
         if reaction != "None":
             if use_correction and reaction not in reaction_blacklist:
-                rdmg = occurence_table[reaction][damage]
-                corrected = max(rdmg, key=rdmg.get)
+                rdmg = frequency_table[reaction][damage]
+                corrected = max(rdmg, key = rdmg.get)
 
                 if attacker != corrected:
-                    uid = row[col.uid]
-                    print(f"\t[UID {uid}] Corrected {reaction} source: {attacker} -> {corrected}")
+                    print(f"[UID {row[col.uid]}] Corrected {reaction} source: {attacker} → {corrected}")
                     attacker = corrected
+                    corrected = True
             
-            damage_table[attacker][reaction]["dmg"] += damage
             ownership_table[reaction][attacker] += 1
+            add2dt(reaction)
         
         else:
             damage_table[attacker]["Crit"][row[col.crit]] += 1
-            source = row[col.source]
-            damage_table[attacker][source]["dmg"] += damage
+            if row[col.amp_type] != "None": add2dt(row[col.amp_type])
+            add2dt(row[col.source])
 
-            amp_type = row[col.amp_type]
-            if amp_type == "None": continue
-            damage_table[attacker][amp_type]["dmg"] += damage
+    if ownership_table:
+        corrected_str = " (Corrected)" if corrected else ""
+        print(f"\nReaction Ownership:{corrected_str}") # redundant but this is just for better visibility
+        print("\n".join(f"{reaction} = {dict2str(owners)}" for reaction, owners in ownership_table.items()))
 
     time = total_time / 1000
     dps = total_damage / time if time > 0 else 0
-    print(f"\n\tTotal Damage: {round(total_damage):,}, Time: {time}s, DPS: {round(dps):,}")
+    print(f"\nTotal Damage: {round(total_damage):,}, Time: {time}s, DPS: {round(dps):,}")
 
     for avatar, stats in damage_table.items():
         avatar_dmg = stats["Total"]["dmg"] 
-        print(f"\t{avatar}: {round(avatar_dmg):,} ({round(avatar_dmg / total_damage * 100, 2)}%)")
+        print(f"\n{avatar}: {round(avatar_dmg):,} ({round(avatar_dmg / total_damage * 100, 2)}%)")
         
         for stat, val in stats.items():   
             match stat:
@@ -131,40 +132,30 @@ def parse_log(lines, use_correction):
                     hits = val["true"]
                     total = hits + val["false"]
                     rate = hits / total * 100 if total > 0 else 0
-                    print(f"\t\t{stat} Rate: {hits}/{total} ({round(rate, 2)}%)")
+                    print(f"{stat} Rate: {hits}/{total} ({round(rate, 2)}%)")
                     continue        
                 case _:
-                    dmg = val["dmg"]
-                    print(f"\t\t{stat}: {round(dmg):,} ({round(dmg / avatar_dmg * 100, 2)}%)")
+                    dmg, count = val["dmg"], val["count"]
+                    dmg_ratio = dmg / avatar_dmg * 100
+                    print(f"{count} {stat}: {round(dmg):,} ({round(dmg_ratio, 2)}%)")
 
-    print("\n\tReaction Ownership:")
-    for reaction, owners in ownership_table.items():
-        print(f"\t{reaction}:")
-        for avatar, count in owners.items():
-            print(f"\t\t{avatar}: {count}")
-
-rot_n = 0
 def main():
     global rot_n
-    fname, lines, err = try_open(arg(0)) # try to open log from provided arg
+    first_line = input("\nEnter log file path or paste logs directly:\n")
+    fname, lines, err = try_open(first_line)
 
-    if not lines: # try to open log from input if no/invalid first arg
-        if arg(0): print(err) # print error if arg was provided
-        first_line = input("\nEnter log file path or paste logs directly:\n")
-        fname, lines, err = try_open(first_line)
-
-        if not lines: # try to determine if input was a path or a pasted log
-            # there should be a better way to do this but logs will never contain slashes anyway
-            if re.search(r"\/|\\", first_line):
-                print(err) # print error if input was path
-                return # start over
-                
-            fname = "pasted log"
-            lines.append(first_line) # (assumed pasted log at this point) append first line of paste
-            while True: # handle pasted newlines, break loop and start processing after empty input
-                line = input()
-                if not line: break
-                lines.append(line.strip())
+    if not lines: # try to determine if input was a path or a pasted log
+        # there should be a better way to do this but logs should never contain slashes anyway
+        if re.search(r"\/|\\", first_line):
+            print(err) # print error if input was path
+            return # start over
+            
+        fname = "pasted log"
+        lines.append(first_line) # (assumed pasted log at this point) append first line of paste
+        while True: # handle pasted newlines, break loop and start processing after empty input
+            line = input()
+            if not line: break
+            lines.append(line.strip())
 
     # group lines into smaller lists if multiple teams are present to handle multi-rotation logs
     team_idxs = [i for i, v in enumerate(lines) if v.startswith("TEAM")]
@@ -175,11 +166,9 @@ def main():
         team = (rot[0][5:].replace(",", ", ")
                 if rot[0].startswith("TEAM")
                 else "(no team header)")
-        print(f"\nRotation {i + rot_n}: {team}")
-        # always asking might be annoying but I think this is for the better atm
-        # hopefuly this entire thing can be deprecated once the logger's accuracy improves
+        print("\n" + "-" * 69 + f"\nRotation {i + rot_n}: {team}")
         ask_rc_str = "Use reaction correction? (Y/N, default N): "
-        try: parse_log(rot, str2bool(arg(1) or input(ask_rc_str)))
+        try: parse_log(rot, str2bool(args.rc or input(ask_rc_str)))
         except Exception: logging.error(traceback.format_exc())
     
     if rotations:
@@ -189,4 +178,8 @@ def main():
         print("Insufficient input.")
 
 if __name__ == "__main__":
-    while True: main(); args.clear()
+    parser = argparse.ArgumentParser()
+    rc_desc = "sets global setting for using reaction correction (stops prompts)"
+    parser.add_argument("-rc", metavar="Y/N", help=rc_desc)
+    rot_n, args = 0, parser.parse_args()
+    while True: main()
